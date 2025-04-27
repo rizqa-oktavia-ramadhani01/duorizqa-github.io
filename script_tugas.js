ef80fba74f6e74d87a2625cd5819e6952ec30732
rizqa-oktavia-ramadhani01 <rizqaoktaviaramadhani@gmail.com>
2025-04-27 12:35:47 +0800

    Add files via upload

--- script_tugas.js
+++ script_tugas.js
@@ -1,9 +1,9 @@
 fetch('data_tugas.json')
   .then(response => response.json())
   .then(data => {
+    // Menampilkan data anggota tim
     const teamGrid = document.getElementById('teamGrid');
-
-    data.forEach(member => {
+    data.profile.forEach(member => {
       if (member.keanggotaan || member.sebagai) {
         const memberDiv = document.createElement('div');
         memberDiv.classList.add('team-member');
@@ -19,10 +19,19 @@ fetch('data_tugas.json')
                 <p><strong>Nama:</strong><br>${member.nama}</p>
                 <p><strong>Sebagai:</strong><br>${member.sebagai}</p>
             </div>
-            `;
+        `;
 
         teamGrid.appendChild(memberDiv);
       }
+    });
+
+    // Menampilkan foto pada Coding Skill section
+    const codingSkillContainer = document.querySelector('.coding-skill .skill-content');
+    data.skill.forEach(skill => {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.innerHTML = `<img src="${skill.codingSkill}" alt="Coding Skill">`;
+      codingSkillContainer.appendChild(box);
     });
   })
   .catch(error => {
